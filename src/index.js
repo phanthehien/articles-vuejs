39ce611a283860c60f754f30ea0e0b559c2eeba0
Phan The Hien <phanthehien@gmail.com>
2018-11-27 16:56:14 +0700

    feature(Article): integrate click button

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-const ArticleListComponent = Vue.extend({
-  template: '#articleList'
-});
-
 Vue.component(
   'article-component',
   {
@@ -10,6 +6,7 @@ Vue.component(
       return {
         numberOfList: 2,
         pagingValues: [1, 2, 3, 5, 7, 9],
+        currentPaging: 0
       }
     },
     props: ['articles'],
@@ -17,6 +14,57 @@ Vue.component(
       numberOfListLabel() {
         return `Number of Items: ${this.numberOfList}`;
       },
+      paginationItems() {
+        const paginationItems = [];
+
+        if (this.articles && this.numberOfList) {
+          const numberOfPaging = Math.ceil(this.articles.length / this.numberOfList);
+
+          for (let i = 0; i < numberOfPaging; i += 1) {
+            paginationItems.push(i + 1);
+          }
+        }
+
+        return paginationItems;
+      },
+      currentArticles() {
+        const skip = this.currentPaging * this.numberOfList;
+        const endIndex = Math.min(skip + this.numberOfList, this.articles.length);
+
+        const articleData = this.articles.slice(skip, endIndex).map((article) => {
+          const newArticle = article;
+          newArticle.shortContent = util.getSubString(article.content);
+          newArticle.author = article.author;
+          newArticle.email = article.email;
+          newArticle.title = article.title;
+          newArticle.content = article.content;
+
+          return newArticle;
+        });
+
+        return articleData;
+      }
+    },
+    methods: {
+      handlePaginationClick() {
+        if (event.target.dataset) {
+          const { index, action } = event.target.dataset;
+
+          switch (action) {
+            case 'actionDropdown':
+              this.numberOfList = parseInt(index);
+              this.currentPaging = 0;
+              break;
+
+            case 'actionPagination':
+              this.currentPaging = parseInt(index - 1);
+              break;
+
+            default:
+              break;
+          }
+        }
+      }
     }
   }
 );
@@ -26,21 +74,6 @@ new Vue({
   data: {
     numberOfList: 2,
     pagingValues: [1, 2, 3, 5, 7, 9],
+    articles: db.articles
   },
-  computed: {
-    articles() {
-      const articleData = db.articles.map((article) => {
-        const newArticle = article;
-        newArticle.shortContent = util.getSubString(article.content);
-        newArticle.author = article.author;
-        newArticle.email = article.email;
-        newArticle.title = article.title;
-        newArticle.content = article.content;
-
-        return newArticle;
-      });
-
-      return articleData;
-    }
-  }
 });
